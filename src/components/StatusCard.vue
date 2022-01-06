87aa41f8ce2f8bea0d286ccc457b3e4c17c8c341
Tajveer Nijjar <Tajveer.Nijjar@connexionz.com>
2022-01-06 13:34:48 +1300

    Created custom Status Dot component.

--- src/components/StatusCard.vue
+++ src/components/StatusCard.vue
@@ -1,17 +1,22 @@
 <template>
   <v-card max-height="250" height="200" class="rounded-lg">
+    <v-icon> mdi-signal-cellular-3 </v-icon>
     <p>Cellular network</p>
-    <p>Status -</p>
+    <status-dot />
   </v-card>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
+import StatusDot from "@/components/StatusDot.vue";
+
 export default Vue.extend({
+  components: {
+    StatusDot
+  },
   data() {
     return {};
   }
 });
 </script>
-<style lang="scss" scoped></style>
---
+++ src/components/StatusDot.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="outer-circle">
+      <div class="inner-circle"></div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.container {
+  margin: 0;
+  padding: 4px;
+  border-radius: 10px;
+  height: 20px;
+  width: 20px;
+  display: inline-block;
+  //   display: flex;
+  //   justify-content: center;
+  //   align-items: center;
+}
+
+.outer-circle {
+  margin: 0 auto;
+  display: block;
+  background: rgba(143, 182, 72, 0.3);
+  border-radius: 16px;
+  height: 24px;
+  width: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.inner-circle {
+  height: 10px;
+  width: 10px;
+  border-radius: 10px;
+  background: rgba(143, 182, 72, 1);
+}
+</style>
