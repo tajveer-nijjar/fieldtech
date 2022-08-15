14dc27749f764d599518587fe65e473089226ee3
Tajveer Nijjar <Tajveer.Nijjar@connexionz.com>
2022-08-15 14:09:57 +1200

    MonitorControl done.

--- src/views/MonitorControl/Components/Monitor.vue
+++ src/views/MonitorControl/Components/Monitor.vue
@@ -1,7 +1,43 @@
 <template>
   <div>
     <ContentPage>
-      <div style="background: red">Monitorrr</div>
+      <div class="px-4 py-3 fill-height overflow-y-auto overflow-x-hidden">
+        <div class="content">
+          <PageHeader :headerTitle="`Monitor`" :headerSubtitle="``" />
+          <template>
+            <v-row>
+              <v-col cols="12" lg="8" xl="12">
+                <div class="text-subtitle-2 small-line-height text-uppercase">
+                  Message Filter
+                </div>
+                <div class="filters">
+                  <v-chip
+                    class="ma-2"
+                    close
+                    :value="index"
+                    :key="`chip-${index}`"
+                    @click:close="handleCloseFilter(index, item)"
+                    v-for="(item, index) in filterItems"
+                  >
+                    {{ item }}
+                  </v-chip>
+                  <template>
+                    <v-btn plain color="accent" class="ml-lg-5" small>
+                      <v-icon small> mdi-plus </v-icon>
+                      Add filter
+                    </v-btn>
+                  </template>
+                </div>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" lg="8" xl="12">
+                <MonitorStatusList :statuses="statuses" />
+              </v-col>
+            </v-row>
+          </template>
+        </div>
+      </div>
     </ContentPage>
   </div>
 </template>
@@ -10,11 +46,40 @@
 import Vue from "vue";
 
 import ContentPage from "@/components/Page/ContentPage.vue";
+import PageHeader from "@/components/Page/PageHeader.vue";
+import MonitorStatusList from "./MonitorStatusList.vue";
 
 export default Vue.extend({
   name: "Monitor",
   components: {
-    ContentPage
+    ContentPage,
+    PageHeader,
+    MonitorStatusList
+  },
+  data() {
+    return {
+      filterItems: ["Alarm", "NPC", "Driver"],
+      statuses: [
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" },
+        { date: "18/06/2023 04:15:18", status: "GPS Good fix" }
+      ]
+    };
+  },
+  methods: {
+    handleCloseFilter(index: number, item: string) {
+      this.filterItems.splice(index, 1);
+    }
   }
 });
 </script>
---
+++ src/views/MonitorControl/Components/MonitorStatusList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    Monitor status list
+    <div class="content">
+      <v-card elevation="0" class="dark-card">
+        <v-card-text
+          class="dark-card-text d-flex"
+          :value="index"
+          :key="`status-${index}`"
+          v-for="(status, index) in statuses"
+        >
+          <div>
+            {{ status.date }}
+          </div>
+          <div class="monitor-status">
+            {{ status.status }}
+          </div>
+        </v-card-text>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "MonitorStatusList",
+  props: {
+    statuses: { type: Array }
+  }
+});
+</script>
+
+<style lang="scss">
+.dark-card {
+  background: #212121 !important;
+  height: calc(100vh - 300px);
+}
+
+.dark-card-text {
+  color: white !important;
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+
+.monitor-status {
+  margin-left: 64px !important;
+}
+</style>
